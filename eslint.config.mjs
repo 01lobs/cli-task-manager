1e793120494de01299ec6b875758e5d685206ec7
01lobs <es-abdelrahmanragab2025@alexu.edu.eg>
2026-02-28 05:32:01 +0200

    Initial project setup: TypeScript, ESLint, Prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import eslint from "@eslint/js";
+import tseslint from 'typescript-eslint';
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ['src/**/*.ts'],
+        rules: {
+        'no-console': 'warn',
+        '@typescript-eslint/no-unused-vars': 'error',
+        },
+    },
+    {
+        ignores: ['dist/' ,'node_modules/']
+    }
+
+);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log('Hello TypeScript on Node.js!');
+console.log('node.js version :', process.version);
+console.log('current directory :', process.cwd());
+console.log('platform :', process.platform);
+console.log("today's date is :", new Date().toLocaleDateString());
